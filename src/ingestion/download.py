665aa7592ac4bf10177e99397b50acdc1b3f0f8d
sam <sfoxroyston@gmail.com>
2018-06-06 00:13:38 -0400

    tests passing after prject structure reformat

---
+++ src/ingestion/download.py
@@ -0,0 +1,238 @@
+import uuid
+from datetime import datetime
+from subprocess import Popen, PIPE
+
+import bs4
+import pandas as pd
+import requests
+from dateutil import parser
+
+from constants import *
+from storage import generate_s3_key, date_from_str, load_configs_from_file
+from storage import s3
+
+
+def ohio_get_last_updated():
+    html = requests.get("https://www6.sos.state.oh.us/ords/f?p=VOTERFTP:STWD", verify=False).text
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    results = soup.find_all("td", {"headers": "DATE_MODIFIED"})
+    return max(parser.parse(a.text) for a in results)
+
+
+def get_object(key, fn):
+    with open(fn, "w+") as obj:
+        s3.Bucket(S3_BUCKET).download_fileobj(Key=key, Fileobj=obj)
+
+
+class Loader(object):
+    """
+    this object should be used to perform downloads directly from online resources which are specified by yaml files
+    in the config directory.
+
+    A Loader uses filesystem resources for temporarily storing the files on disk during the chunk concatenation process,
+    therefore __enter__ and __exit__ are defined to allow safe usage of Loader in a 'with' statement:
+    for example:
+    ```
+        with Loader() as l:
+            l.download_chunks()
+            l.s3_dump()
+    ```
+    """
+
+    def __init__(self, config_file=CONFIG_OHIO_FILE, force_date=None, force_file=None, clean_up_tmp_files=True):
+        self.config_file_path = config_file
+        self.clean_up_tmp_files = clean_up_tmp_files
+        config = load_configs_from_file(config_file=config_file)
+        self.config = config
+        self.chunk_urls = config[CONFIG_CHUNK_URLS] if CONFIG_CHUNK_URLS in config else []
+        if "tmp" not in os.listdir("/"):
+            os.system("mkdir /tmp")
+        self.file_type = config["file_type"]
+        self.source = config["source"]
+        self.is_compressed = False
+        self.checksum = None
+        self.state = config["state"]
+        self.obj_will_download = False
+
+        if force_date is not None:
+            self.download_date = parser.parse(force_date).isoformat()
+        else:
+            self.download_date = datetime.now().isoformat()
+
+        if force_file is not None:
+            self.main_file = force_file
+        else:
+            self.main_file = "/tmp/voteshield_{}.tmp".format(uuid.uuid4())
+
+        self.temp_files = [self.main_file]
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.clean_up_tmp_files:
+            self.clean_up()
+
+    def clean_up(self):
+        for fn in self.temp_files:
+            if os.path.isfile(fn):
+                os.remove(fn)
+        self.temp_files = []
+
+    def download_src_chunks(self):
+        """
+        we expect each chunk to be a compressed (.gz) csv file
+        :return:
+        """
+        self.obj_will_download = True
+        if os.path.isfile(self.main_file):
+            os.remove(self.main_file)
+        first_success = False
+        for i, url in enumerate(self.chunk_urls):
+            logging.info("downloading chunk {} from {}".format(i, url))
+            chunk_storage = "{}.{}.gz".format(self.main_file, str(i))
+            with open(chunk_storage, "w+") as f:
+                dl_proc = Popen(["curl", "--insecure", "-X", "GET", url], stdout=f, stderr=PIPE)
+                dl_proc.communicate()
+                dl_proc.wait()
+
+            p = Popen(["gunzip", chunk_storage], stdout=PIPE, stderr=PIPE)
+            p.communicate()
+            p.wait()
+            decompressed_chunk = ".".join(chunk_storage.split(".")[:-1])
+            try:
+                df = pd.read_csv(decompressed_chunk)
+                s = df.to_csv(header=not first_success)
+                first_success = True
+                logging.info("done with chunk {}".format(i))
+            except ValueError:
+                logging.warning("malformed response from {}".format(url))
+                continue
+
+            with open(self.main_file, 'a+') as f:
+                f.write(s)
+            self.temp_files.append(decompressed_chunk)
+
+        self.compress()
+        self.compute_checksum()
+        self.download_date = datetime.now().isoformat()
+        return self.checksum
+
+    def compute_checksum(self):
+        logging.info("calculating checksum")
+        p = Popen(["cksum", self.main_file], stdout=PIPE, stderr=PIPE)
+        out, err = p.communicate()
+        self.checksum = out
+        return self.checksum
+
+    def compress(self, compression_type="gzip"):
+        if not self.is_compressed:
+            logging.info("compressing")
+            p = Popen([compression_type, self.main_file], stdout=PIPE, stderr=PIPE)
+            p.communicate()
+            self.main_file += ".gz"
+            self.is_compressed = True
+
+    def decompress(self, compression_type="gunzip"):
+        if self.is_compressed:
+            logging.info("decompressing {}".format(self.main_file))
+            if compression_type == "unzip":
+                logging.info("decompressing unzip {} to {}".format(self.main_file, os.path.dirname(self.main_file)))
+                p = Popen([compression_type, self.main_file, "-d", os.path.dirname(self.main_file)],
+                          stdout=PIPE, stderr=PIPE)
+            else:
+                logging.info("decompressing gunzip {} to {}".format(self.main_file, os.path.dirname(self.main_file)))
+                p = Popen([compression_type, self.main_file], stdout=PIPE, stderr=PIPE)
+            p.communicate("A")
+            logging.info("decompressing done".format(self.main_file))
+            if self.main_file[-3:] == ".gz":
+                self.main_file = self.main_file[:-3]
+            self.is_compressed = False
+
+    def generate_key(self, file_class=PROCESSED_FILE_PREFIX):
+        return generate_s3_key(file_class, self.state, self.source,
+                               self.download_date, self.file_type, self.is_compressed)
+
+    def s3_dump(self, file_class=PROCESSED_FILE_PREFIX):
+        if self.config["state"] == 'ohio' and self.obj_will_download:
+            self.download_date = ohio_get_last_updated().isoformat()
+    
+        with open(self.main_file) as f:
+            self.main_file = "/tmp/voteshield_{}.tmp".format(uuid.uuid4())
+            s3.Object(S3_BUCKET, self.generate_key(file_class=file_class))\
+                .put(Body=f, Metadata={"last_updated": self.download_date})
+
+
+class Preprocessor(Loader):
+    def __init__(self, raw_s3_file, config_file=CONFIG_OHIO_FILE, clean_up_tmp_files=True):
+        super(Preprocessor, self).__init__(config_file=config_file,
+                                           force_date=date_from_str(raw_s3_file),
+                                           clean_up_tmp_files=clean_up_tmp_files)
+        self.raw_s3_file = raw_s3_file
+        self.s3_download()
+
+    def s3_download(self):
+        get_object(self.raw_s3_file, self.main_file)
+
+    def preprocess_generic(self):
+        logging.info("preprocessing generic")
+        self.decompress()
+
+    def preprocess_nevada(self):
+        logging.info("preprocessing nevada")
+        dir_path = os.path.dirname(self.main_file)
+        before = set(os.listdir(dir_path))
+        self.decompress(compression_type="unzip")
+        after = set(os.listdir(dir_path))
+        new_files = list(after.difference(before))
+        new_files = ["{}/{}".format(dir_path, n) for n in new_files]
+        voter_file = new_files[0] if "ElgbVtr" in new_files[0] else new_files[1]
+        hist_file = new_files[0] if "VtHst" in new_files[0] else new_files[1]
+        self.temp_files.extend([hist_file, voter_file])
+        logging.info("NEVADA: loading historical file")
+        df_hist = pd.read_csv(hist_file, header=None)
+        df_hist.columns = self.config["hist_columns"]
+        logging.info("NEVADA: loading main voter file")
+        df_voters = pd.read_csv(voter_file, header=None)
+        df_voters.columns = self.config["ordered_columns"]
+        valid_elections = df_hist.date.unique().tolist()
+        valid_elections.sort(key=lambda x: datetime.strptime(x, "%m/%d/%Y"))
+
+        def place_vote_hist(g):
+            group_idx = 0
+            output = []
+            for d in valid_elections[::-1]:
+                if group_idx < len(g.date.values) and d == g.date.values[group_idx]:
+                    output.append(g.vote_code.values[group_idx])
+                    group_idx += 1
+                else:
+                    output.append('n')
+
+            return output
+
+        def fast_dummy(g):
+            return list(g.date)
+
+        voting_histories = df_hist.groupby(self.config["voter_id"]).apply(place_vote_hist)
+        df_voters = df_voters.set_index(self.config["voter_id"])
+        df_voters["all_history"] = voting_histories
+        self.main_file = "/tmp/voteshield_{}.tmp".format(uuid.uuid4())
+        df_voters.to_csv(self.main_file)
+        self.temp_files.append(self.main_file)
+        chksum = self.compute_checksum()
+        return chksum
+
+    def execute(self):
+        self.state_router()
+
+    def state_router(self):
+        routes = {'nevada': self.preprocess_nevada}
+        if self.config["state"] in routes:
+            f = routes[self.config["state"]]
+            f()
+        else:
+            self.preprocess_generic()
+
+
+if __name__ == '__main__':
+    print(ohio_get_last_updated())
---
+++ src/ingestion/file_diagnostics.py
@@ -0,0 +1,76 @@
+import inspect
+import json
+import os
+import uuid
+from os import stat
+
+from analysis import Snapshot, SnapshotConsistencyError
+from storage import get_preceding_upload
+from storage import load_configs_from_file
+
+
+class DiagnosticTest(object):
+    """
+    This class gets used to ensure that each uploaded snapshot is consistent with it's configuration file and with past
+    snapshots. We run this check before inserting the new changes into the modifications table and it's descendants.
+    """
+    def __init__(self, file_path, config_file, preproc_obj):
+        self.file_path = file_path
+        self.configs = load_configs_from_file(config_file=config_file)
+        self.preproc_obj = preproc_obj
+        self.logs = ""
+        self.config_file = config_file
+        if "tmp" not in os.listdir("/"):
+            os.system("mkdir /tmp")
+
+    def log_msg(self, msg):
+        try:
+            f_name = inspect.stack()[1][3]
+        except IndexError:
+            f_name = "main"
+        self.logs += "{}: {}\n".format(f_name, msg)
+
+    def test_file_size(self):
+        fchange_threshold = 0.15
+        df = get_preceding_upload(self.configs["state"], self.preproc_obj.download_date)
+        preceding_upload = json.loads(df.to_json(orient="records"))
+        if len(preceding_upload) == 0:
+            self.log_msg("-- PASSED -- since there is no preceding file, there is no filesize check")
+            return True
+        else:
+            preceding_upload = preceding_upload[0]
+            last_fs = preceding_upload["size"]
+            this_fs = stat(self.file_path).st_size
+            bigger_file = last_fs if last_fs > this_fs else this_fs
+            smaller_file = last_fs if last_fs <= this_fs else this_fs
+            fchange = (bigger_file - smaller_file) / bigger_file
+            if fchange > fchange_threshold:
+                if bigger_file == last_fs:
+                    self.log_msg("-- FAILED -- compressed file shrank by {}% (>{}%) since last upload"
+                                 .format(fchange, fchange_threshold * 100))
+                else:
+                    self.log_msg("-- FAILED -- compressed file grew by {}% (>{}%) since last upload"
+                                 .format(fchange, fchange_threshold * 100))
+                return False
+            else:
+                self.log_msg("-- PASSED -- compressed file sizes changed by {}% (<{}%)".format(fchange,
+			     fchange_threshold * 100))
+		return True
+
+    def test_snapshots_dryrun(self):
+        tmp_test_file = "/tmp/voteshield_head_{}_{}.csv".format(uuid.uuid4(), self.preproc_obj.download_date)
+        self.preproc_obj.temp_files.append(tmp_test_file)
+        os.system("head -1000 {} > {}".format(self.file_path, tmp_test_file))
+        print(self.file_path)
+        try:
+            test_s = Snapshot(config_file=self.config_file, file_name=tmp_test_file)
+            self.log_msg("-- PASSED --")
+            return True
+        except SnapshotConsistencyError as e:
+            self.log_msg("-- FAILED -- " + str(e))
+            return False
+
+    def run_all_tests(self):
+        t0 = self.test_file_size()
+        t1 = self.test_snapshots_dryrun()
+        return all([t0, t1]), self.logs
