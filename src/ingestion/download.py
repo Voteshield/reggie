60512b42f1a1d47d2474525a025b70542379f517
danny <dswift333@gmail.com>
2018-10-10 11:05:14 -0400

    NAN party

--- src/ingestion/download.py
+++ src/ingestion/download.py
@@ -682,7 +682,7 @@ class Preprocessor(Loader):
 
         vdf[config["voter_id"]] = vdf[config["voter_id"]]\
             .astype(int, errors='ignore')
-        vdf["party_identifier"] = ""
+        vdf["party_identifier"] = "NAN"
         hdf2.sort_values(by=["Voter_ID"], inplace=True)
 
         def get_history(row):
