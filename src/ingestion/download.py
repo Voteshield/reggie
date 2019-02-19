d3c58e06c05fddb8958dc2f5a3f1b6d5720ea5d6
tylerjrichards <tylerjrichards@gmail.com>
2019-02-19 16:35:00 -0500

    added string matching

--- src/ingestion/download.py
+++ src/ingestion/download.py
@@ -840,14 +840,22 @@ class Preprocessor(Loader):
 
         self.config = Config("north_carolina")
         for i in new_files:
-            if "ncvhis" in i['name'] and "MACOSX" not in i['name']:
+            print(i)
+            if "ncvhis" in i['name'] and ".txt" in i['name'] and "MACOSX" not in i['name']:
                 vote_hist_file = i
-            elif "ncvoter" in i['name'] and "MACOSX" not in i['name']:
+            elif "ncvoter" in i['name'] and ".txt" in i['name'] and "MACOSX" not in i['name']:
                 voter_file = i
+        print(voter_file)
+        print(vote_hist_file)
         voter_df = pd.read_csv(voter_file['obj'], sep="\t",
                                quotechar='"')
         vote_hist = pd.read_csv(vote_hist_file['obj'], sep="\t",
                                 quotechar='"')
+        print(voter_df.head())
+        print(vote_hist.head())
+        print("columns")
+        print(voter_df.columns)
+        print(self.config['ordered_columns'])
 
         voter_df.columns = self.config["ordered_columns"]
         vote_hist.columns = self.config["hist_columns"]
