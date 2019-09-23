a8da5b327ebeda394934b624de89c0e0f2cee0ea
cvcotton <cvcotton@gmail.com>
2019-09-23 17:24:31 -0400

    State/nevada switch ids (#363)
    * Switched nevada main id from 'state_id' -> 'county_id'.
    * Brought nevada's vote-history code up to date: histories are now stored in the standard form
    * fixed bug introduced by new get trends stuff involved with cache

--- src/ingestion/download.py
+++ src/ingestion/download.py
@@ -868,42 +868,44 @@ class Preprocessor(Loader):
             else new_files[1]
         hist_file = new_files[0] if "VtHst" in new_files[0]["name"] else \
             new_files[1]
-        logging.info("NEVADA: loading historical file")
+
         df_hist = pd.read_csv(hist_file["obj"], header=None)
         df_hist.columns = self.config["hist_columns"]
-        logging.info("NEVADA: loading main voter file")
         df_voters = pd.read_csv(voter_file["obj"], header=None)
         df_voters.columns = self.config["ordered_columns"]
-        valid_elections = df_hist.date.unique().tolist()
+
+        sorted_codes = df_hist.date.unique().tolist()
+        sorted_codes.sort(key=lambda x: datetime.strptime(x, "%m/%d/%Y"))
         counts = df_hist.date.value_counts()
-        valid_elections.sort(key=lambda x: datetime.strptime(x, "%m/%d/%Y"))
+        sorted_codes_dict = {k: {"index": i,
+                                 "count": int(counts.loc[k]),
+                                 "date": date_from_str(k)}
+                             for i, k in enumerate(sorted_codes)}
 
-        # NOTE: this function only works correctly if
-        # df_hist is assumed to be sorted by date
-        def place_vote_hist(g):
-            group_idx = 0
-            output = []
-            for d in valid_elections[::-1]:
-                if group_idx < len(g.date.values) and \
-                        d == g.date.values[group_idx]:
-                    output.append(g.vote_code.values[group_idx])
-                    group_idx += 1
-                else:
-                    output.append('n')
-            return output
+        def insert_code_bin(arr):
+            if isinstance(arr, list):
+                return [sorted_codes_dict[k]["index"] for k in arr]
+            else:
+                return np.nan
 
-        voting_histories = df_hist.groupby(self.config["voter_id"])\
-            .apply(place_vote_hist)
-        df_voters["tmp_id"] = df_voters[self.config["voter_id"]]
-        df_voters = df_voters.set_index("tmp_id")
-        df_voters["all_history"] = voting_histories
+        df_voters = df_voters.set_index('VoterID', drop=False)
+        voter_id_groups = df_hist.groupby('VoterID')
+        df_voters['all_history'] = voter_id_groups['date'].apply(list)
+        df_voters['votetype_history'] = voter_id_groups['vote_code'].apply(list)
+        df_voters['sparse_history'] = df_voters['all_history'].map(insert_code_bin)
+
+        # create compound string for unique voter ID from county ID
+        df_voters['County_Voter_ID'] = df_voters['County'].str.replace(
+            ' ', '').str.lower() + '_' + df_voters['County_Voter_ID'].astype(
+            int).astype(str)
+
         df_voters = self.config.coerce_dates(df_voters)
-        df_voters = self.config.coerce_numeric(df_voters)
+        df_voters = self.config.coerce_numeric(df_voters, extra_cols=['Zip',
+            'Phone', 'Congressional_District', 'Senate_District',
+            'Assembly_District', 'Education_District', 'Regent_District',
+            'Registered_Precinct'])
+        df_voters = self.config.coerce_strings(df_voters)
 
-        sorted_codes = valid_elections
-        sorted_codes_dict = {k: {"index": i, "count": int(counts.loc[k]),
-                         "date": date_from_str(k)}
-                     for i, k in enumerate(sorted_codes)}
         self.meta = {
             "message": "nevada_{}".format(datetime.now().isoformat()),
             "array_encoding": json.dumps(sorted_codes_dict),
