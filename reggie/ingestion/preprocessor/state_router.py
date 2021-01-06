8f89161d5e57b5fb7767cf50d5fa80e341e18628
Tommi Tsuruga <tommi.tsuruga@gmail.com>
2021-01-06 12:38:46 -0500

    cleaning up preprocessor, added states

--- reggie/ingestion/preprocessor/state_router.py
+++ reggie/ingestion/preprocessor/state_router.py
@@ -1,13 +1,65 @@
+import logging
+
 from reggie.ingestion.download import Preprocessor
 from reggie.ingestion.preprocessor.iowa_preprocessor import PreprocessIowa
+from reggie.ingestion.preprocessor.arizona2_preprocessor import (
+    PreprocessArizona2,
+)
+
+# Check the function paramaters here, some might not need to be set here or need better names
+# Need to pull out: Alaska
 
 
-#needs some type of driver file to avoid circular imports
+def state_router(
+    state,
+    raw_s3_file,
+    config_file,
+    force_date=None,
+    force_file=None,
+    testing=False,
+    ignore_checks=False,
+    s3_bucket="",
+    **kwargs
+):
+    # if state == 'iowa':
+    #     f = PreprocessIowa(raw_s3_file=raw_s3_file, config_file=config_file, force_date=None, force_file=None,
+    #              testing=False, ignore_checks=False, s3_bucket=s3_bucket, **kwargs)
+    #     print(f.processed_file)
+    print("before routes in state_router: ", state, raw_s3_file, config_file)
 
-def state_router(state, raw_s3_file, config_file, force_date=None, force_file=None,
-                 testing=False, ignore_checks=False, s3_bucket="", **kwargs):
-    if state == 'iowa':
-        f = PreprocessIowa(raw_s3_file=raw_s3_file, config_file=config_file, force_date=None, force_file=None,
-                 testing=False, ignore_checks=False, s3_bucket=s3_bucket, **kwargs)
-        print(f.processed_file)
-    return f
+    # (
+    #     raw_s3_file=raw_s3_file,
+    # config_file=config_file,
+    # force_date=None,
+    # force_file=None,
+    # testing=False,
+    # ignore_checks=False,
+    # s3_bucket=s3_bucket,
+    # ** kwargs
+    # ),
+    routes = {
+        "arizona2": PreprocessArizona2,
+        "iowa": PreprocessIowa,
+    }
+    if state in routes:
+        # F is now some typs of class object?
+        logging.info(
+            "state: {}, config: {}, main file: {}".format(
+                state, config_file, raw_s3_file
+            )
+        )
+        f = routes[state](raw_s3_file=raw_s3_file,
+            config_file=config_file,
+            force_date=None,
+            force_file=None,
+            testing=False,
+            ignore_checks=False,
+            s3_bucket=s3_bucket,
+            **kwargs)
+        logging.info("preprocessing {}".format(state))
+        return f
+    else:
+        raise NotImplementedError(
+            "preprocess_{} has not yet been "
+            "implemented for the Preprocessor object".format(state)
+        )
