33cb6c9f68a4d26f52b826ed56f3b3c80f5ab892
Alan Palazzolo <github@zzolo.org>
2021-02-17 16:44:40 -0500

    Updates dependencies to be more specific.

--- setup.py
+++ setup.py
@@ -4,27 +4,29 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='reggie',
-    version='0.1',
-    author="Tyler Richards",
-    author_email="tylerjrichards@gmail.com",
+    name="reggie",
+    version="0.1.0",
+    author="VoteShield, Tyler Richards, and contributors",
+    author_email="support@voteshield.us",
     description="A package for reading in Voter files",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=["pandas",
-                      "click",
-                      "numpy",
-                      "zipfile2",
-                      "PyYAML<4.3,>=3.10",
-                      "boto3",
-                      "requests<2.21,>=2.20.0",
-                      "xlrd",
-                      "bs4"],
+    install_requires=[
+        "pandas>=0.23.1",
+        "click>=7.1.2",
+        "numpy>=1.19.2",
+        "zipfile2>=0.0.12",
+        "PyYAML>=5.3.1",
+        "boto3==1.15.12",
+        "requests<2.21,>=2.20.0",
+        "xlrd>=1.2.0",
+        "bs4>=0.0.1",
+    ],
     url="https://github.com/Voteshield/reggie",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    entry_points='''
+    entry_points="""
     [console_scripts]
     reg=reggie:convert_cli
-    '''
+    """,
 )
