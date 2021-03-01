e792ba6987e21efff238a9bcd666dc1a5598052a
Alan Palazzolo <190733+zzolo@users.noreply.github.com>
2021-03-01 16:18:07 -0500

    Pulling CLI out of main.py.

---
+++ reggie/cli.py
@@ -0,0 +1,38 @@
+"""
+Click-based CLI for Reggie processing.
+"""
+
+from reggie.configs.configs import Config
+from reggie.ingestion.preprocessor.state_router import state_router
+from reggie.main import convert_voter_file
+import datetime
+import click
+
+
+@click.command(help="convert a non-standard voter file")
+@click.option(
+    "--state", required=True, default=None, help="U.S. state name: e.g. florida"
+)
+@click.option(
+    "--local_file",
+    required=True,
+    default=None,
+    help="location and name of file: e.g. 'FL_2019-01-01.zip'",
+)
+@click.option(
+    "--file_date",
+    required=True,
+    default=None,
+    help="date of voter file in format 'YYYY-MM-DD'",
+)
+@click.option("--write_file", required=False, default=True, is_flag=True)
+def convert_cli(state, local_file, file_date, write_file):
+    if file_date is None:
+        file_date = datetime.datetime.today().date().isoformat()
+    convert_voter_file(
+        state=state, local_file=local_file, file_date=file_date, write_file=write_file
+    )
+
+
+if __name__ == "__main__":
+    convert_cli()
--- reggie/main.py
+++ reggie/main.py
@@ -1,35 +1,26 @@
+"""
+Main Reggie file that provides the method to convert voter files.
+"""
+
 from reggie.configs.configs import Config
 from reggie.ingestion.preprocessor.state_router import state_router
 import datetime
-import click
 
 
-def convert_voter_file(state=None, local_file=None,
-                       file_date=None, write_file=False):
+def convert_voter_file(state=None, local_file=None, file_date=None, write_file=False):
     config_file = Config.config_file_from_state(state)
-    file_date = str(datetime.datetime.strptime(file_date, '%Y-%m-%d').date())
-    preprocessor = state_router(None, raw_s3_file=None,
-                     config_file=config_file,
-                     force_file=local_file,
-                     force_date=file_date)
+    file_date = str(datetime.datetime.strptime(file_date, "%Y-%m-%d").date())
+    preprocessor = state_router(
+        None,
+        raw_s3_file=None,
+        config_file=config_file,
+        force_file=local_file,
+        force_date=file_date,
+    )
     preprocessor.execute()
     if not write_file:
-        return(preprocessor.output_dataframe(preprocessor.processed_file),
-               preprocessor.meta)
+        return (
+            preprocessor.output_dataframe(preprocessor.processed_file),
+            preprocessor.meta,
+        )
     preprocessor.local_dump(preprocessor.processed_file)
-
-
-@click.command(help="convert a non-standard voter file")
-@click.option("--state", required=True, default=None,
-              help="U.S. state name: e.g. florida")
-@click.option("--local_file", required=True, default=None,
-              help="location and name of file: e.g. 'FL_2019-01-01.zip'")
-@click.option("--file_date", required=True,
-              default=None,
-              help="date of voter file in format 'YYYY-MM-DD'")
-@click.option("--write_file", required=False, default=True, is_flag=True)
-def convert_cli(state, local_file, file_date, write_file):
-    if file_date is None:
-        file_date = datetime.datetime.today().date().isoformat()
-    convert_voter_file(state=state, local_file=local_file,
-                       file_date=file_date, write_file=write_file)
