e268aaac1be044dd94cf0e9302516251366e774e
Tommi Tsuruga <tommi.tsuruga@gmail.com>
2021-03-08 17:57:52 -0500

    refactor to much faster mapping

--- reggie/ingestion/preprocessor/pennsylvania_preprocessor.py
+++ reggie/ingestion/preprocessor/pennsylvania_preprocessor.py
@@ -55,39 +55,16 @@ class PreprocessPennsylvania(Preprocessor):
             dfcols.extend(["election_{}_party".format(i + 1)])
         dfcols.extend(config["ordered_columns"][-3:])
 
-        # helper
-        def return_election_string(x, election_dict_map):
-            elect_map = election_dict_map.copy()
-            no_nan_cols = x.dropna()
-            hist_list = []
-            for i, value in enumerate(no_nan_cols):
-                hist_string = ""
-                election_key = no_nan_cols.index[i].split('_')[1]
-                election = "_".join(no_nan_cols.index[i].split('_')[:2])
-                if int(election_key) in elect_map.keys():
-                    hist_string += elect_map[int(election_key)]
-                    hist_string += " "
-                    elect_map.pop(int(election_key))
-                    hist_string += f'{no_nan_cols[election + "_vote_method"]} '
-                    hist_string += f'{no_nan_cols[election + "_party"]}'
-                if hist_string != "":
-                    hist_list.append(hist_string)
-            return hist_list
-
-        # more help
-        def return_district_string(x, zone_dict):
-            no_nan = x.dropna()
-            zone_list = []
-            zone_string = ""
-            for idx, value in enumerate(no_nan):
-                zone_string = ""
-                try:
-                    zone_string += f'{zone_dict[str(no_nan[idx])]}'
-                except KeyError:
-                    zone_string = ""
-                if zone_list != "":
-                    zone_list.append(zone_string)
-            return zone_list
+        # help, python maps are so much faster
+        def list_map(df_sub, columns, zone_dict=None):
+            def mapping(li, zone_dict=zone_dict):
+                if zone_dict is None:
+                    li = [x for x in li if x != 'nan']
+                    return li
+                else:
+                    li = [zone_dict[x] for x in li if x != 'nan' and x in zone_dict]
+                    return li
+            return pd.Series(map(mapping, df_sub[columns].values.astype(str).tolist()))
         # for c in counties:
         for idx, c in enumerate(counties):
             logging.info("Processing {}".format(c))
@@ -127,22 +104,43 @@ class PreprocessPennsylvania(Preprocessor):
                 names=["county", "number", "abbr", "title"],
                 error_bad_lines=False,
             )
-            # vote_history = df.iloc[:, 70:110]
-            # vote_columns = vote_history.columns.to_list()
-            vote_columns = df.columns[70:110].to_list()
-            district_columns = df.columns[30:70].to_list()
+            # format the election data to merge
             edf["election_list"] = edf["title"] + " " + edf["date"]
+
+            vote_columns = df.columns[70:150].to_list()
+            district_columns = df.columns[30:70].to_list()
+
+            # create a dict of the election data and the number in the given file, this corresponds to the column
+            # location in the file
             election_map = pd.Series(edf.election_list.values, index=edf.number).to_dict()
-            # vectorize if time or possible?
-            df["all_history"] = df[vote_columns].apply(return_election_string, args=(election_map,), axis=1)
+            # merge the zone files together
             unholy_union = zdf.merge(tdf, how='left', on='zone_number')
+            # format a column that contains the zone description and the name so that it matches the current district
+            # field
             unholy_union["combined"] = unholy_union["zone_description"] + " Type: " + unholy_union["zone_long_name"]
-            zone_dict = dict(zip(unholy_union.zone_code, unholy_union.combined))
-            df["all_history"] = df[vote_columns].apply(return_election_string, args=(election_map,), axis=1)
-            df["districts"] = df[district_columns].apply(return_district_string, args=(zone_dict,), axis=1)
-            # for i in range(elections):
-            #     df = df.drop("election_{}".format(i), axis=1)
-            #     df = df.drop("district_{}".format(i + 1), axis=1)
+
+            # create a dict that contains the zone code as the key and the long name string as the value
+            zone_dict = dict(zip(unholy_union.zone_code.astype(str), unholy_union.combined))
+            # gathers the pairs of election columns to iterate over both at the same time
+            vote_li = list(zip(df.columns[70:110:2], df.columns[71:110:2]))
+            district_df = df[district_columns]
+            # get the value from the eleciton map key, then combine it with the value in the party and vote type cells
+            vote_hist_df = pd.DataFrame({i: election_map[int(i.split("_")[1])] + " " + df[i] + " " + df[j] for i, j in vote_li})
+
+            # Unnecessary
+            cols_to_drop = vote_hist_df.columns
+            # converts the dataframe to a series that contains the list of elections participate in indexed on position
+            vote_hist_df = list_map(vote_hist_df, cols_to_drop)
+            district_ser = list_map(district_df, district_columns, zone_dict)
+
+            df['all_history'] = vote_hist_df
+            df['districts'] = district_ser
+            df.drop(vote_columns, axis=1, inplace=True)
+            df.drop(district_columns, axis=1, inplace=True)
+
+            #     df["all_history"] = df[vote_columns].apply(return_election_string, args=(election_map,), axis=1)
+            #     df["districts"] = df[district_columns].apply(return_district_string, args=(zone_dict,), axis=1)
+            # return df
             # can check columns for each PA county?
             self.column_check(list(df.columns))
             if main_df is None:
