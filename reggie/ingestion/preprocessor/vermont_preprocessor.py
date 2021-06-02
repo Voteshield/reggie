e67092d3e1ad3c9413ab6f9b117393e03915a114
Tommi Tsuruga <tommi.tsuruga@gmail.com>
2021-06-02 13:35:17 -0400

    a bit of cleanup and speedup

--- reggie/ingestion/preprocessor/vermont_preprocessor.py
+++ reggie/ingestion/preprocessor/vermont_preprocessor.py
@@ -35,67 +35,75 @@ class PreprocessVermont(Preprocessor):
         if self.raw_s3_file is not None:
             self.main_file = self.s3_download()
 
+        def hist_map(df, columns):
+            def mapping(li):
+                li = [x for x in li if x != "nan"]
+                return li
+            return pd.Series(map(mapping, df[columns].values.astype(str).tolist()))
+
         new_files = self.unpack_files(self.main_file, compression="unzip")
         voter_file = [
             n for n in new_files if "voter file" in n["name"].lower()
         ][0]
 
-        df_voter = pd.read_csv(voter_file["obj"], sep="|", dtype=str).iloc[
-            :, : len(self.config["column_names"])
-        ]
-        df_voter.columns = self.config["column_names"]
+        vdf = pd.read_csv(voter_file["obj"], sep="|", dtype=str)
+        unnamed_cols = vdf.columns[vdf.columns.str.contains("Unnamed")]
+        vdf.drop(columns=unnamed_cols, inplace=True)
 
-        df_hist = df_voter.loc[
-            :, [self.config["voter_id"]] + self.config["election_columns"]
-        ]
-        for c in df_hist.columns[1:]:
-            df_hist.loc[:, c] = df_hist.loc[:, c].map(
-                {"T": c[: c.find(" Part")].replace(" ", "_"), "F": np.nan}
-            )
+        # Will probably need to drop election columns for snapshot differencer
+        election_columns = [col for col in vdf.columns if 'election' in col.lower()]
+        rename_dict = {col: col.replace(" Participation", "").replace(" ", "_") for col in election_columns}
 
-        df_hist = (
-            df_hist.set_index(self.config["voter_id"])
-            .stack()
-            .reset_index(level=1, drop=True)
-            .reset_index()
-        )
-        df_hist.columns = [self.config["voter_id"], "all_history"]
+        vdf.rename(columns=rename_dict, inplace=True)
 
-        elections, counts = np.unique(df_hist.all_history, return_counts=True)
+        #Just need to sort once
+        election_columns = sorted(list(rename_dict.values()))
+        # Replacing the boolean values in the cells with the election name for processing
+        for c in list(rename_dict.values()):
+            vdf.loc[:, c] = vdf.loc[:, c].map(
+                {"T": c.replace(" ", "_"), "F": np.nan})
 
-        sorted_elections_dict = {
-            k: {"index": i, "count": int(counts[i]), "date": str(k[:4])}
-            for i, k in enumerate(elections)
+        election_counts = vdf[election_columns].count()
+        sorted_codes_dict = {
+            election_counts.index[i]: {
+                "index": i,
+                "count": k,
+                "date": str(
+                    datetime.strptime(election_counts.index[i][:4], "%Y")
+                        .date()
+                        .strftime("%m/%d/%Y")
+                ),
+            }
+            for i, k in enumerate(election_counts)
         }
-        sorted_elections = list(sorted_elections_dict.keys())
+        sorted_elections = list(sorted_codes_dict.keys())
 
-        df_hist.loc[:, "sparse_history"] = df_hist.all_history.apply(
-            lambda x: sorted_elections_dict[x]["index"]
+        vdf['all_history'] = hist_map(vdf[election_columns], election_columns)
+
+        def insert_code_bin(arr):
+            if isinstance(arr, list):
+                return [sorted_codes_dict[k]["index"] for k in arr]
+            else:
+                return np.nan
+
+        vdf.loc[:, "sparse_history"] = vdf.loc[:, "all_history"].map(
+            insert_code_bin
         )
 
-        group = df_hist.groupby(self.config["voter_id"])
-        df_hist = pd.concat(
-            [group[col].apply(list) for col in df_hist.columns[1:]], axis=1
-        )
+        vdf = vdf.set_index(self.config["voter_id"])
 
-        df_voter = df_voter.loc[
-            :, ~df_voter.columns.isin(self.config["election_columns"])
-        ]
-        df_voter = df_voter.set_index(self.config["voter_id"])
-
-        df_voter = self.config.coerce_strings(df_voter)
-        df_voter = self.config.coerce_numeric(df_voter)
-        df_voter = self.config.coerce_dates(df_voter)
-        df_voter = df_voter.join(df_hist)
+        vdf = self.config.coerce_strings(vdf)
+        vdf = self.config.coerce_numeric(vdf)
+        vdf = self.config.coerce_dates(vdf)
 
         self.meta = {
             "message": "vermont_{}".format(datetime.now().isoformat()),
-            "array_encoding": json.dumps(sorted_elections_dict),
+            "array_encoding": json.dumps(sorted_codes_dict),
             "array_decoding": json.dumps(sorted_elections),
         }
 
         self.processed_file = FileItem(
             name="{}.processed".format(self.config["state"]),
-            io_obj=StringIO(df_voter.to_csv(encoding="utf-8", index=True)),
+            io_obj=StringIO(vdf.to_csv(encoding="utf-8", index=True)),
             s3_bucket=self.s3_bucket,
         )
