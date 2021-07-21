c8b9d1bc6fce9572d2dd89d1bd938b00faeee31f
Tommi-Ann Tsuruga <tommi.tsuruga@gmail.com>
2021-07-21 14:40:19 -0400

    checking tracemalloc

--- reggie/ingestion/preprocessor/wisconsin_preprocessor.py
+++ reggie/ingestion/preprocessor/wisconsin_preprocessor.py
@@ -11,7 +11,7 @@ from io import StringIO
 import numpy as np
 from datetime import datetime
 import gc
-
+import tracemalloc
 
 class PreprocessWisconsin(Preprocessor):
     def __init__(self, raw_s3_file, config_file, force_date=None, **kwargs):
@@ -29,6 +29,7 @@ class PreprocessWisconsin(Preprocessor):
         self.processed_file = None
 
     def execute(self):
+        tracemalloc.start()
         if self.raw_s3_file is not None:
             self.main_file = self.s3_download()
 
@@ -200,7 +201,9 @@ class PreprocessWisconsin(Preprocessor):
         # need to tell it it's not compressed for test files otherwise it creates a malformed object
         # self.is_compressed = False
         logging.info("Wisconsin: writing out")
-
+        snapshot = tracemalloc.take_snapshot()
+        for stat in snapshot.statistics("lineno"):
+            logging.info(stat)
         self.processed_file = FileItem(
             name="{}.processed".format(self.config["state"]),
             io_obj=StringIO(main_df.to_csv(encoding="utf-8", index=False)),
