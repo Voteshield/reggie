3a95c72e018dafbb6f98fc1e8c7942c2779bb6ba
Tommi-Ann Tsuruga <tommi.tsuruga@gmail.com>
2021-08-25 16:10:44 -0400

    cleanup

--- reggie/ingestion/preprocessor/wisconsin_preprocessor.py
+++ reggie/ingestion/preprocessor/wisconsin_preprocessor.py
@@ -13,6 +13,7 @@ from datetime import datetime
 import gc
 import chardet
 
+
 class PreprocessWisconsin(Preprocessor):
     def __init__(self, raw_s3_file, config_file, force_date=None, **kwargs):
 
@@ -45,71 +46,53 @@ class PreprocessWisconsin(Preprocessor):
         else:
             main_file = new_files[0]
 
-        # logging.info("buffer size: {}".format(main_file["obj"].__sizeof__()))
-        rawdata = main_file["obj"].read(100000)
-        result = chardet.detect(rawdata)
-        encoding_result = result['encoding']
-
-        if encoding_result == 'ascii':
-            # I have no idea why I keep needing to do this?
-            encoding_result = 'latin-1'
+        chardata = main_file["obj"].read(100000)
+        result = chardet.detect(chardata)
+        encoding_result = result["encoding"]
+        main_file["obj"].seek(0)
         logging.info("encoding: {}".format(encoding_result))
-        main_file["obj"].seek(0)
-        # todo: add the other format here
-        # todo: try and except this because there are so many encodings
-        if encoding_result != 'latin-1':
-            try:
-                wi_columns = pd.read_csv(
-                    main_file["obj"], sep="\t", nrows=0
-                ).columns.tolist()
-            except UnicodeDecodeError:
-                logging.info('unicode error in the latin-1?')
-                main_file["obj"].seek(0)
-                wi_columns = pd.read_csv(
-                    main_file["obj"], sep=",", nrows=0, encoding='latin-1'
-                ).columns.tolist()
-        else:
-            main_file["obj"].seek(0)
-            try:
-                wi_columns = pd.read_csv(
-                    main_file["obj"], sep=",", encoding='latin-1', nrows=0
-                ).columns.tolist()
-            except UnicodeDecodeError:
-                logging.info('in the exception')
-                main_file["obj"].seek(0)
-                wi_columns = pd.read_csv(
-                    main_file["obj"], sep="\t", nrows=0
-                ).columns.tolist()
-            if len(wi_columns) == 1:
-                logging.info('incorrect separator, but which encoding?')
-                main_file["obj"].seek(0)
-                wi_columns = pd.read_csv(
-                    main_file["obj"], sep="\t", nrows=0, encoding='latin-1'
-                ).columns.tolist()
-        # wi_columns = pd.read_csv(
-        #     main_file["obj"], sep="\t", nrows=0
-        # ).columns.tolist()
-        main_file["obj"].seek(0)
-        # Todo: add to yaml instead of here,
-        cat_columns = [
-            "Suffix",
-            "UnitType",
-            "Congressional",
-            "State Senate",
-            "Court of Appeals",
-            "Jurisdiction",
-            "High School",
-            "Technical College",
-            "Representational School",
-            "State",
-            "First Class School",
-            "Incorporation",
-            "voter_status",
-            "Voter Status Reason",
-            "Voter Type",
-            "IsPermanentAbsentee",
-        ]
 
+        if encoding_result == "ascii":
+            # the ascii format causes an pandas decoding error, latin-1 is the correct encoding
+            encoding_result = "latin-1"
+        else:
+            # utf-8 is the pandas default and is the encoding for some files, usually the comma-separated files but not
+            # always
+            encoding_result = "utf-8"
+        file_sep = "\t"
+        # Some files are tab separated and some are comma separated,
+        # if encoding_result != 'latin-1':
+        try:
+            wi_columns = pd.read_csv(
+                main_file["obj"],
+                sep=file_sep,
+                nrows=0,
+                encoding=encoding_result,
+            ).columns.tolist()
+        except UnicodeDecodeError:
+            # Attempt Latin-1
+            main_file["obj"].seek(0)
+            wi_columns = pd.read_csv(
+                main_file["obj"], sep=file_sep, nrows=0, encoding="latin-1"
+            ).columns.tolist()
+            encoding_result = "latin-1"
+
+        # If the number of columns is only 1, the wrong was used and it is (probably) a csv file, read in the correct
+        # columns with the encoding that works
+        if len(wi_columns) == 1:
+            main_file["obj"].seek(0)
+            file_sep = ","
+            wi_columns = pd.read_csv(
+                main_file["obj"],
+                sep=file_sep,
+                nrows=0,
+                encoding=encoding_result,
+            ).columns.tolist()
+
+        main_file["obj"].seek(0)
+        cat_columns = self.config["categorical_columns"]
+
+        # Helper function to determine if a column is a history column by checking it for a date
         def parse_histcols(col_name):
             try:
                 parser.parse(col_name)
@@ -125,6 +108,8 @@ class PreprocessWisconsin(Preprocessor):
                 valid_elections.append(column)
 
         cat_columns.extend(valid_elections)
+
+        # Specify categorical columns to save memory
         dtype_dict = {
             col: ("str" if col not in cat_columns else "category")
             for col in wi_columns
@@ -163,8 +148,7 @@ class PreprocessWisconsin(Preprocessor):
             main_df["Voter Reg Number"].astype(str).str.isdigit()
         ]
 
-        # standardize La Crosse County and rename it to La Crosse County
-        # Todo: Check for other places/spellings might need regex
+        # standardize LaCrosse County and rename it to La Crosse County
         main_df.loc[
             main_df["County"].str.lower() == "lacrosse county", "County"
         ] = "La Crosse County"
@@ -179,7 +163,7 @@ class PreprocessWisconsin(Preprocessor):
             )
         )
 
-        # self.column_check(list(set(main_df.columns) - set(valid_elections)))
+        self.column_check(list(set(main_df.columns) - set(valid_elections)))
         # sort from oldest election available to newest
         valid_elections = sorted(
             valid_elections, key=lambda date: parser.parse(date)
@@ -237,12 +221,8 @@ class PreprocessWisconsin(Preprocessor):
             "array_encoding": sorted_codes_dict,
             "array_decoding": sorted_codes,
         }
-        # need to tell it it's not compressed for test files otherwise it creates a malformed object
-        # self.is_compressed = False
+
         logging.info("Wisconsin: writing out")
-        # snapshot = tracemalloc.take_snapshot()
-        # for stat in snapshot.statistics("lineno"):
-        #     logging.info(stat)
         df_csv = main_df.to_csv(encoding="utf-8", index=False)
         del main_df
         gc.collect()
