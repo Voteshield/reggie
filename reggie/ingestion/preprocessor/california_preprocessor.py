38670d4f31dac70875825c637f7eaa76d4a33eba
Tommi-Ann Tsuruga <tommi.tsuruga@gmail.com>
2021-09-21 18:52:44 -0400

    changed dtypes

--- reggie/ingestion/preprocessor/california_preprocessor.py
+++ reggie/ingestion/preprocessor/california_preprocessor.py
@@ -373,8 +373,7 @@ class PreprocessCalifornia(Preprocessor):
             "Status",
             "VoterStatusReasonCodeDesc",
             "AssistanceRequestFlag",
-            "VbmVoterType",
-            "PrecinctId",
+            "VbmVoterType"
         ]
         # read in columns to set dtype as pyarrow
         col_ifornia = pd.read_csv(
@@ -425,7 +424,7 @@ class PreprocessCalifornia(Preprocessor):
             "CI": "Municipality",
             "SU": " County Supervisoral",
         }
-        district_df = pd.read_csv(district_file["obj"], sep="\t")
+        district_df = pd.read_csv(district_file["obj"], sep="\t", dtype=string[pyarrow])
 
         logging.info("Read in District DF")
         prof_num += 1
