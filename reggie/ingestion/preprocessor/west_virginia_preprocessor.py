a02e0a1c9a7d1b689f63cb2c5ce2d07996285e1b
Tommi-Ann Tsuruga <tommi.tsuruga@gmail.com>
2021-12-22 18:12:35 -0500

    renaming to match older version suffix

--- reggie/ingestion/preprocessor/west_virginia_preprocessor.py
+++ reggie/ingestion/preprocessor/west_virginia_preprocessor.py
@@ -95,7 +95,8 @@ class PreprocessWestVirginia(Preprocessor):
         # adds dummy column to wv to match format of older files
         if "mail unit" not in df_voters.columns.str.lower():
             df_voters["MAIL UNIT"] = np.nan
-        df_voters.rename(columns={"COUNTY_NAME": "County_Name", "STATUS": "Status"}, inplace=True, errors="ignore")
+
+        df_voters.rename(columns={"COUNTY_NAME": "County_Name", "STATUS": "Status", "SUFFIX": "Suffix"}, inplace=True, errors="ignore")
         # Add county id column
         df_voters.insert(
             1,
