087146e9636f75a3d690e369766b24653835c21b
Courtenay Cotton <cvcotton@gmail.com>
2023-09-15 14:00:09 -0400

    Bugfixes for GA file changes (#153)

--- reggie/ingestion/preprocessor/georgia_preprocessor.py
+++ reggie/ingestion/preprocessor/georgia_preprocessor.py
@@ -204,7 +204,7 @@ class PreprocessGeorgia(Preprocessor):
 
             # Convert county back to numbers to match existing system
             county_dict = self.config.primary_locale_names[self.config.primary_locale_type]
-            county_dict = {v.lower().replace(" ",""): int(k) for k, v in county_dict.items()}
+            county_dict = {v.lower().replace(" ",""): str(int(k)) for k, v in county_dict.items()}
             df_voters["County_code"] = df_voters["County_code"].str.lower().map(county_dict)
 
             # Convert voter status to match existing system
