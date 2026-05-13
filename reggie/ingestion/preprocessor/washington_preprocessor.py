fa7e7c75e65dcdf72dc7eb8339b984c2d950a08c
Courtenay Cotton <courtenay.cotton@voteshield.us>
2026-05-13 14:31:54 -0400

    WA changed voter file name (#263)

--- reggie/ingestion/preprocessor/washington_preprocessor.py
+++ reggie/ingestion/preprocessor/washington_preprocessor.py
@@ -51,8 +51,9 @@ class PreprocessWashington(Preprocessor):
         # there should be only one voter file
         voter_file = [
             n for n in new_files
-            if "vrdb" in n["name"].lower()
-            or "voterregistrationexport" in n["name"].lower()
+            if ("vrdb" in n["name"].lower())
+            or ("voterregistrationexport" in n["name"].lower())
+            or ("wa.txt" in n["name"].lower())
         ][0]
         hist_files = [n for n in new_files if "history" in n["name"].lower()]
 
