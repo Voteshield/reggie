a9d7b8e32e86c4a8738da47fd69caa68d2fa6bd7
Courtenay Cotton <courtenay.cotton@voteshield.us>
2026-06-26 17:23:33 -0400

    almost finished

--- reggie/ingestion/preprocessor/alaska_preprocessor.py
+++ reggie/ingestion/preprocessor/alaska_preprocessor.py
@@ -15,6 +15,10 @@ from reggie.ingestion.download import (
     FileItem,
 )
 
+from reggie.ingestion.utils import (
+    UnknownCityError,
+)
+
 
 class PreprocessAlaska(Preprocessor):
     def __init__(self, raw_s3_file, config_file, force_date=None, **kwargs):
@@ -53,7 +57,7 @@ class PreprocessAlaska(Preprocessor):
             # Active voter file should be the only csv
             active_file = new_files[0]
             logging.info(
-                f"Found active voter file: {active_voter_file}"
+                f"Found active voter file: {active_file['name']}, "
                 f"and no inactive voter file."
             )
         else:
@@ -63,44 +67,58 @@ class PreprocessAlaska(Preprocessor):
                 n for n in new_files if n != inactive_file["name"]
             ][0]
             logging.info(
-                f"Found active voter file: {active_voter_file}"
-                f"and inactive voter file: {inactive_voter_file}"
+                f"Found active voter file: {active_file['name']}, "
+                f"and inactive voter file: {inactive_file['name']}"
             )
 
-        df_voter = pd.read_csv(active_file["obj"])
+        df_voter = self.read_csv_count_error_lines(
+            active_file["obj"],
+            sep=",",
+            on_bad_lines="warn",
+        )
         df_voter["STATUS"] = "active"
 
         if inactive_file:
-            df_inactive = pd.read_csv(inactive_file["obj"])
+            df_inactive = self.read_csv_count_error_lines(
+                inactive_file["obj"],
+                sep=",",
+                on_bad_lines="warn",
+            )
             df_inactive["STATUS"] = "inactive"
             df_voter = pd.concat([df_voter, df_inactive], axis=0)
             df_voter.reset_index(drop=True, inplace=True)
 
         # Normalize: sometimes column names are missing underscore
         # or are otherwise irregular from file to file
+        df_voter.rename(
+            columns=self.config["column_aliases"],
+            inplace=True,
+        )
         for c in df_voter.columns:
             df_voter.rename(
                 columns={c: c.replace(" ", "_")},
                 inplace=True
         )
-        df_voter.rename(
-            columns=self.config["column_aliases"],
-            inplace=True,
-        )
 
         # Add dummy birth date column, to prevent errors
         df_voter["BIRTH_DATE"] = None
 
+        # Add borough column, which we will populate with a lookup later
+        df_voter["BOROUGH"] = None
+
         # If no inactive columns, add them, to match 2-file version
         for c in ["CONDITION_DATE", "CC"]:
             if c not in df_voter.columns:
-                df[c] = None
+                df_voter[c] = None
 
         # Split out "state house district" and "precinct" into
         # 2 separate columns.
         # They are contained together in "DP" column.
         df_voter["STATE_HOUSE_DISTRICT"] = df_voter["DP"].str.split("-")[0][0]
+        df_voter["STATE_HOUSE_DISTRICT"] = df_voter["STATE_HOUSE_DISTRICT"].map(lambda x: str(int(x)))
         df_voter["PRECINCT"] = df_voter["DP"].str.split("-")[0][1]
+        df_voter["PRECINCT"] = df_voter["PRECINCT"].map(lambda x: str(int(x)))
+        df_voter.drop(columns=["DP"], inplace=True)
 
         # Party codes N and U both correspond to "no party",
         # so consolidate them both as U.
@@ -108,34 +126,36 @@ class PreprocessAlaska(Preprocessor):
             lambda x: "U" if x == "N" else x
         )
 
+        # Vote history
+        hist_columns = [f"VH{x}" for x in range(1,17)]
 
+        # Check that we have ended up with all the expected columns
+        self.column_check(list(set(df_voter.columns) - set(hist_columns)))
 
-        # todo finish...
+        def collect_history_codes(column_data, election_code=True):
+            codes = []
+            for c in column_data:
+                if (c is not np.nan) and (c is not None):
+                    # Make election codes slightly more reader-friendly
+                    if election_code:
+                        elec = c.split()[0]
+                        codes.append("20" + elec[:2] + "_" + elec[2:])
+                    else:
+                        codes.append(c.split()[1])
+            return codes
 
-
-        # --- handling the vote history file --- #
-
-        df_hist = (
-            df_hist.set_index(self.config["voter_id"])
-            .stack()
-            .reset_index()
-            .iloc[:, [0, 2]]
+        df_voter["all_history"] = df_voter[hist_columns].apply(
+            lambda x: collect_history_codes(x, election_code=True),
+            axis=1,
         )
-        df_hist.columns = [self.config["voter_id"], "election"]
-
-        df_hist = df_hist.join(df_hist.election.str.split(" ", expand=True))
-        df_hist = df_hist.rename(
-            {0: "all_history", 1: "votetype_history"}, axis=1
+        df_voter["votetype_history"] = df_voter[hist_columns].apply(
+            lambda x: collect_history_codes(x, election_code=False),
+            axis=1,
         )
 
-        df_hist = df_hist.join(
-            df_hist.all_history.str.split("(?<=^\d{2})", expand=True)
-        )
-        df_hist = df_hist.rename(
-            {0: "election_year", 1: "election_type"}, axis=1
-        )
 
-        df_hist.election_year = "20" + df_hist.election_year
+        # Alaska primary is always mid-August, so assign Aug 15 for primary dates
+        # All other elections, we have no way of knowing, so stick them in the middle of the year ? July 1 ?
 
         elections, counts = np.unique(
             df_hist.loc[:, ["all_history", "election_year"]].apply(
@@ -148,42 +168,61 @@ class PreprocessAlaska(Preprocessor):
             k[0]: {"index": i, "count": int(counts[i]), "date": int(k[1])}
             for i, k in enumerate(elections)
         }
+
+
+
         sorted_elections = list(sorted_elections_dict.keys())
 
         df_hist.loc[:, "sparse_history"] = df_hist.all_history.map(
             lambda x: sorted_elections_dict[x]["index"]
         )
 
-        df_hist = pd.concat(
-            [
-                df_hist.groupby(self.config["voter_id"])[c].apply(list)
-                for c in ["all_history", "votetype_history", "sparse_history"]
-            ],
-            axis=1,
-        )
 
-        # --- handling the voter file --- #
 
-        df_voter = df_voter.loc[
-            :, ~df_voter.columns.isin(self.config["election_columns"])
-        ]
-        df_voter = df_voter.set_index(self.config["voter_id"])
 
         df_voter = self.config.coerce_strings(df_voter)
         df_voter = self.config.coerce_numeric(df_voter)
         df_voter = self.config.coerce_dates(df_voter)
 
-        df_voter = df_voter.join(df_hist)
+        # Map cities to boroughs / census areas:
+        def borough_lookup(row):
+            city = row["RESIDENCE_CITY"]
+            # There are ~50k null residence cities, but almost no null mailing cities.
+            # Fallback to mailing city if residence city is unusable
+            if (row["RESIDENCE_CITY"] is np.nan) or (row["RESIDENCE_CITY"] is None) or (
+                row["RESIDENCE_CITY"] in ['federal', 'fedeal', 'overseas', '-']):
+                city = row["MAILING_CITY"]
+            if (city is np.nan) or (city is None) or (
+                city in ['federal', 'fedeal', 'overseas', '-']):
+                return np.nan
+            if city in self.config["cities_to_boroughs"]:
+                return self.config["cities_to_boroughs"][city]
+            else:
+                raise UnknownCityError(
+                    f"Encountered unknown city ("{city}") in Alaska, "
+                    f"that is not associated with a known borough or census area. "
+                    f"Please add {city} to the cities_to_boroughs dictionary in alaska.yaml, "
+                    f"and then reprocess this file."
+                )
 
-        # Check the file for all the proper locales
-        self.locale_check(
-            set(df_voter[self.config["primary_locale_identifier"]]),
+        df_voter["BOROUGH"] = df_voter[["RESIDENCE_CITY", "MAILING_CITY"]].apply(
+            borough_lookup, axis=1
         )
+
+        # Reorder voter columns
+        df_voter = [
+            self.config(["ordered_columns"]) + self.config(
+                ["ordered_generated_columns"]
+            )
+        ]
+
+        # Set voter ID as index
+        df_voter = df_voter.set_index(self.config["voter_id"])
 
         self.meta = {
             "message": "alaska_{}".format(datetime.now().isoformat()),
-            "array_encoding": json.dumps(sorted_elections_dict),
-            "array_decoding": json.dumps(sorted_elections),
+            "array_encoding": json.dumps(sorted_codes_dict),
+            "array_decoding": json.dumps(sorted_codes),
         }
 
         self.processed_file = FileItem(
